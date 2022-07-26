81cb4babd81460b8014e4f85d56964c9e4dc3328
Tri Denda <tridenda.nk@gmail.com>
2022-07-27 03:21:21 +0700

    Convert directory item, button, icon, cart dropdown scss to jsx using styled components

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop/*" element={<Shop />} />
-        <Route path="authentication" element={<Authentication />} />
+        <Route path="auth" element={<Authentication />} />
         <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>

081e94c2d15acb39496122819066648f582601ad
Tri Denda <tridenda.nk@gmail.com>
2022-08-02 20:03:51 +0700

    migrate to styled components and remove sass

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -5,7 +5,7 @@ import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
 import { signUpStart } from "../../store/user/user.action";
 
-import "./sign-up-form.styles.scss";
+import { SignUpContainer } from "./sign-up-form.styles";
 
 const defaultFormFields = {
   displayName: "",
@@ -49,7 +49,7 @@ const SignUpForm = () => {
   };
 
   return (
-    <div className="sign-up-container" onSubmit={handleSubmit}>
+    <SignUpContainer onSubmit={handleSubmit}>
       <h2>Don't have an account?</h2>
       <span>Sign up with your email and password</span>
       <form>
@@ -91,7 +91,7 @@ const SignUpForm = () => {
 
         <Button type="submit">Sign Up</Button>
       </form>
-    </div>
+    </SignUpContainer>
   );
 };
 

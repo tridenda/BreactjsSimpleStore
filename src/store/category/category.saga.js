d6b8b23901af6fb36307efb72dcdff1209b5dd76
Tri Denda <tridenda.nk@gmail.com>
2022-08-04 00:42:57 +0700

    convert category redux to typescript

--- src/store/category/category.saga.js
+++ src/store/category/category.saga.js
@@ -7,7 +7,7 @@ import {
   fetchCategoriesFailed,
 } from "./category.action";
 
-import CATEGORIES_ACTION_TYPES from "./category.types";
+import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
 export function* fetchCategoriesAsync() {
   try {

e005fecd6efccef66bf6ac4bf69301cfea8bbc70
Tri Denda <tridenda.nk@gmail.com>
2022-08-13 00:31:03 +0700

    type up user redux

--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -1,6 +1,6 @@
 import { takeLatest, put, all, call } from "redux-saga/effects";
 
-import USER_ACTION_TYPES from "./user.types";
+import { USER_ACTION_TYPES } from "./user.types";
 
 import {
   signInSuccess,
---
+++ src/store/user/user.selector.ts
@@ -0,0 +1,11 @@
+import { createSelector } from "reselect";
+
+import { UserState } from "./user.reducer";
+
+export const selectUserReducer = (state: { user: UserState }): UserState =>
+  state.user;
+
+export const selectCurrentUser = createSelector(
+  selectUserReducer,
+  (state) => state.currentUser
+);

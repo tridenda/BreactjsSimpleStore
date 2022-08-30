bfecf8df3e6428dd6fe1b98634adc4d5e3ba1655
Tri Denda <tridenda.nk@gmail.com>
2022-08-30 10:40:19 +0700

    added hot-product component

---
+++ src/components/hot-product/hot-product.component.jsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import Button from "../button/button.component";
+import {
+  BigImage,
+  BottomTextContainer,
+  DescriptionText,
+  HotProductContainer,
+  LeftImage,
+  RightImage,
+  TitleText,
+  TopTextContainer,
+} from "./hot-product.styles";
+
+const HotProduct = () => {
+  return (
+    <HotProductContainer>
+      <TopTextContainer>
+        <TitleText>Clothes For Modern Cool Mens</TitleText>
+        <DescriptionText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        </DescriptionText>
+      </TopTextContainer>
+      <BigImage imageUrl="https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1072&q=80" />
+      <RightImage imageUrl="https://i.ibb.co/RvwnBL8/pink-shirt.png" />
+      <LeftImage imageUrl="https://i.ibb.co/mh3VM1f/polka-dot-shirt.png" />
+      <BottomTextContainer>
+        <DescriptionText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        </DescriptionText>
+        <Link to="/shop/mens">
+          <Button>Shop Now</Button>
+        </Link>
+      </BottomTextContainer>
+    </HotProductContainer>
+  );
+};
+
+export default HotProduct;

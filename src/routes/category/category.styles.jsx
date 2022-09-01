64e1bc50224645707174c35e80fbf69a06748e70
Tri Denda <tridenda.nk@gmail.com>
2022-09-01 21:00:40 +0700

    added responsive feature to category-preview and category components

--- src/routes/category/category.styles.jsx
+++ src/routes/category/category.styles.jsx
@@ -5,6 +5,18 @@ export const CategoryContainer = styled.div`
   grid-template-columns: repeat(4, 1fr);
   column-gap: 20px;
   row-gap: 50px;
+
+  @media screen and (max-width: 1130px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media screen and (max-width: 800px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media screen and (max-width: 550px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `;
 
 export const CategoryTitle = styled.h2`
